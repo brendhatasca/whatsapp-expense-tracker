d9b56bfabdea337ad68ca8fcb5cc0b3008a9fd42
Brendha Tasca <brendha.jakubiak@gmail.com>
2026-01-10 12:34:04 -0500

    google sheets integration working

--- src/config/env.js
+++ src/config/env.js
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+
 export const env = {
     twilioAccountSID: process.env.TWILIO_ACCOUNT_SID,
     twilioAuthToken: process.env.TWILIO_AUTH_TOKEN,
@@ -5,5 +7,8 @@ export const env = {
     googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
     googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     googleSheetID: process.env.GOOGLE_SHEET_ID,
-    
+}
+
+for (const [k, v] of Object.entries(env)) {
+  if (!v) throw new Error(`Missing env var for ${k}`);
 }
