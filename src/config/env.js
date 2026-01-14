78d9d4c6a5e35084c8c5eb51c706d043871648bb
Brendha Tasca <brendha.jakubiak@gmail.com>
2026-01-13 21:59:25 -0500

    updated couple variables values

--- src/config/env.js
+++ src/config/env.js
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 
 export const env = {
+    personalPhoneNumber: process.env.PERSONAL_PHONE_NUMBER,
+    port: process.env.PORT || 3000,
     twilioAccountSID: process.env.TWILIO_ACCOUNT_SID,
     twilioAuthToken: process.env.TWILIO_AUTH_TOKEN,
     twilioPhoneNumber: process.env.TWILIO_PHONE_NUMBER,
