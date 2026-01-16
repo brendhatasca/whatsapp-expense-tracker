63f69fe66ddc9aaf8c99235e4bcd747dbfaa90d3
Brendha Tasca <brendha.jakubiak@gmail.com>
2026-01-16 17:04:52 -0500

    updated date helper functions

---
+++ src/utils/dateHelper.js
@@ -0,0 +1,32 @@
+import { parse, format } from 'date-fns';
+
+export function parseDate(monthStr, day) {
+    // take string eg: jan 16 and
+    // return Jan 16 2025
+    const formattedMonthStr = monthStr.charAt(0).toUpperCase() + monthStr.slice(1).toLowerCase();
+    const today = new Date();
+    const currentYear = format(today, 'yyyy');
+
+    const dateWithYear = `${formattedMonthStr} ${day} ${currentYear}`;
+
+    // Use parse() to convert string into a valid Date obj
+    const parsedDateObj = parse(dateWithYear, 'MMM d yyyy', new Date());
+    const parsedDate = format(parsedDateObj, 'MMM d yyyy');
+    const parsedMonth = format(parsedDateObj, 'MMMM');
+
+    return {
+        date: parsedDate,
+        month: parsedMonth
+    };
+}
+
+export function getTodaysMonthAndDate() {
+    const today = new Date();
+    const normalizedDate = format(today, 'MMM d yyyy');
+    const normalizeMonth = format(today, 'MMMM')
+
+    return {
+        month: normalizeMonth,
+        date: normalizedDate
+    }
+}
