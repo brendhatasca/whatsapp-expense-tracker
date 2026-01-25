69d893b0e88539b791b6a52b24e639886a986f03
Brendha Tasca <brendha.jakubiak@gmail.com>
2026-01-25 17:27:23 -0500

    ignore app.js testing file

--- src/domain/transaction.js
+++ src/domain/transaction.js
@@ -22,18 +22,38 @@ function isExpense() {
     //
 }
 
+const CATEGORIES = [
+    'groceries', 'rent', 'food', 'gas', 'transportation',
+    'shopping', 'health', 'entertainment'
+]
+
 export function extractCategoryAndExpense(str) {
     // Extract category and description from string
     // by converting str to array 
     const normalizedStr = str;
-    const wordsArray = normalizedStr.split(" ");
+    const wordsArray = normalizedStr.split("/\s+/");
+    const firstWord = wordsArray[0];
 
-    const category = wordsArray[0];
-    const remainingWords = wordsArray.slice(1);
-    const description = remainingWords.join(" ")
+    if (CATEGORIES.includes(firstWord)) {
+        const category = capitalize(firstWord);
+        const description = wordsArray.slice(1).join(' ') || category;
+        return { category, description };
+    }
 
+    for (const cat of CATEGORIES) {
+        if (str.includes(cat)) {
+            return { 
+                category: capitalize(cat), 
+                description: str 
+            };
+        };
+    };
     return {
         category,
         description
-    }
+    };
 }
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
